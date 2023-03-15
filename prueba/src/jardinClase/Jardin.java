e1f47a314c06134bb8b4a977f327cae769aba602
DjRl0 <115179266+DjRl0@users.noreply.github.com>
2023-03-15 11:49:55 +0100

    Update Jardin.java

--- prueba/src/jardinClase/Jardin.java
+++ prueba/src/jardinClase/Jardin.java
@@ -1,5 +1,12 @@
 package jardinClase;
 
 public class Jardin {
+    private int cont = 0;
+    public void incrementar(){
+        cont++;
+    }
 
+    public int getCont() {
+        return cont;
+    }
 }
